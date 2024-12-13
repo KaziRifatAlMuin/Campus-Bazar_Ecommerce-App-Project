8ad24deb35f614c5e6cc136e214143a8b4b9491a
Kazi Rifat Al Muin <muin2107042@stud.kuet.ac.bd>
2024-12-13 22:33:31 +0600

    Offers Added

--- app/src/main/java/com/rifat/campusbazar/OffersFragment.java
+++ app/src/main/java/com/rifat/campusbazar/OffersFragment.java
@@ -1,13 +1,16 @@
 package com.rifat.campusbazar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 import com.rifat.campusbazar.databinding.FragmentOffersBinding;
 
 public class OffersFragment extends Fragment {
@@ -16,13 +19,31 @@ public class OffersFragment extends Fragment {
     private OffersViewModel viewModel;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         viewModel = new ViewModelProvider(this).get(OffersViewModel.class);
         binding = FragmentOffersBinding.inflate(inflater, container, false);
 
-        RecyclerView recyclerView = binding.offersList;
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        setupClickListeners();
 
         return binding.getRoot();
+    }
+
+    private void setupClickListeners() {
+        binding.banner1.setOnClickListener(v -> navigateToHomeActivity("Offer 1"));
+        binding.banner2.setOnClickListener(v -> navigateToHomeActivity("Offer 2"));
+        binding.banner3.setOnClickListener(v -> navigateToHomeActivity("Offer 3"));
+        binding.banner4.setOnClickListener(v -> navigateToHomeActivity("Offer 4"));
+    }
+
+    private void navigateToHomeActivity(String offerDetails) {
+        Intent intent = new Intent(getActivity(), HomeActivity.class);
+        intent.putExtra("offerDetails", offerDetails);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
